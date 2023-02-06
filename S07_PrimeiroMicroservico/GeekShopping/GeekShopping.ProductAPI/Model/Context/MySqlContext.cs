ec4ee29c762f80f4f0ff28d570657d9e957b0daf
cesarbotas <cesarbotas@gmail.com>
2023-02-06 11:15:07 -0300

    Inclusão da Aula 07 COmpleta

---
+++ S07_PrimeiroMicroservico/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.ProductAPI.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base (options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
