09ff58e2f14857e7fbf9fe9e814aecf877898b9b
Cesar Botas <cesar.botas@modal.com.br>
2023-03-20 17:01:18 -0300

    Aula 08 Completa

--- S08_OrganizandoArquitetura/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_OrganizandoArquitetura/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,5 +1,9 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +14,23 @@ builder.Services.AddDbContext<MySqlContext>(options => options
 
 // Add services to the container.
 
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "GeekShopping.ProductAPI",
+        Version = "v1",
+    });
+});
 
 var app = builder.Build();
 
