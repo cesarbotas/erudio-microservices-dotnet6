fcb6e9d7980b0bbad0e44fed2d87185c551c8509
Cesar Botas <cesar.botas@modal.com.br>
2023-04-05 11:21:18 -0300

    Incluindo a Aula 12

---
+++ S12_ListagemProdutosHome/GeekShopping/GeekShopping.ProductAPI/Migrations/20230327130503_SeedProductDataTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 2L, "T-Shirt", "Descrição", "http://imagemURL.com/Imagem2", "Name", 9.9m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+        }
+    }
+}
