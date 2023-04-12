3a37b77d588e951715eb24e0184b12fa3051db04
cesarbotas <cesarbotas@gmail.com>
2023-04-12 16:04:43 -0300

    Ajuste do Model

--- GeekShoppingProject/GeekShopping/GeekShopping.Web/Models/Cart/CartDetailViewModel.cs
+++ GeekShoppingProject/GeekShopping/GeekShopping.Web/Models/Cart/CartDetailViewModel.cs
@@ -6,7 +6,7 @@
         public long CartHeaderId { get; set; }
         public CartHeaderViewModel CartHeader { get; set; }
         public long ProductId { get; set; }
-        public ProductVO Product { get; set; }
+        public ProductViewModel Product { get; set; }
         public int Count { get; set; }
     }
 }
