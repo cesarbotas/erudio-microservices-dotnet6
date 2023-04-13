803ca8f4d2d523f27dc579fdf2942d8d57355bb6
cesarbotas <cesarbotas@gmail.com>
2023-04-13 13:49:32 -0300

    migrations coupon code

--- GeekShoppingProject/GeekShopping/GeekShopping.CouponAPI/Migrations/20230413164553_SeedCoupounDatabase.Designer.cs
+++ GeekShoppingProject/GeekShopping/GeekShopping.CouponAPI/Migrations/20230413164553_SeedCoupounDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using GeekShopping.CouponAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.CouponAPI.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20230413164553_SeedCoupounDatabase")]
+    partial class SeedCoupounDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
