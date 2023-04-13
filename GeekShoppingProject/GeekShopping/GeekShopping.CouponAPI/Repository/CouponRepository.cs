0f1f1d9243be3d4a383f203cfec5584d28c2c813
Cesar Botas <cesar.botas@modal.com.br>
2023-04-13 13:44:18 -0300

    Criacao dos COupons para Desconto

---
+++ GeekShoppingProject/GeekShopping/GeekShopping.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly MySqlContext _mySqlContext;
+        private IMapper _mapper;
+
+        public CouponRepository(MySqlContext mySqlContext, 
+            IMapper mapper)
+        {
+            _mySqlContext = mySqlContext;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponVO> GetCouponByCouponCode(string couponCode)
+        {
+            var coupon = await _mySqlContext.Coupons.FirstOrDefaultAsync(c => c.CouponCode == couponCode);
+
+            return _mapper.Map<CouponVO>(coupon);
+        }
+    }
+}
