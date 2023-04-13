b5d66dbeeba07924f0e0d385f28c1dfe5bb6744a
Cesar Botas <cesar.botas@modal.com.br>
2023-04-13 12:58:36 -0300

    Inclusão Projeto Aula 16

---
+++ GeekShoppingProject/GeekShopping/GeekShopping.CouponAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,12 @@
+﻿using GeekShopping.CouponAPI.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base (options) { }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
