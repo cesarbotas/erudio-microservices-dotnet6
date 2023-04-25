f143b53f7f069909591699c1d64676effaf64dee
cesarbotas <cesarbotas@gmail.com>
2023-04-25 17:04:48 -0300

    Executando as migraions

--- GeekShoppingProject/GeekShopping/GeekShopping.Email/Program.cs
+++ GeekShoppingProject/GeekShopping/GeekShopping.Email/Program.cs
@@ -1,4 +1,16 @@
+using GeekShopping.Email.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options
+    .UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 25))));
+
+//var builderContext = new DbContextOptionsBuilder<MySqlContext>();
+
+//builderContext.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 25)));
 
 // Add services to the container.
 
